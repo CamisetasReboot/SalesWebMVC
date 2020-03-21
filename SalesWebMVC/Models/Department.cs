519cfcf54ff6075ed58b49e7ae6df3a0b9d2c1f6
Andre Dias <andrelgdias1971@gmail.com>
2020-03-21 20:27:50 -0300

    Banco de dados criado

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
 
 namespace SalesWebMVC.Models
 {
@@ -9,6 +12,29 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public ICollection<Seller> Sellers{ get; set; }=new List<Seller>();
+
+        public Department()
+        {
+            
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
 
     }
 }
